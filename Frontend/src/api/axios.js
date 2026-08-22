8536d6da06ee523d34837bbec3ee40d735912f24
Arib Akhtar <kirmada.789@gmail.com>
2026-08-22 12:31:35 +0530

    Update backend URL for production

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: "https://job-portal-v23h.onrender.com/api",
   withCredentials: true, // Cookies (JWT) ke liye zaroori hai
 });
 
